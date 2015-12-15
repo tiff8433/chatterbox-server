b0be0c957e979f5571a4bd21611270483f4eb2bc
tiff8433 <tiffany.l.huang@gmail.com>
2015-12-15 12:31:11 -0800

    Sets up express

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,42 +1,53 @@
-/* Import node's http module: */
-var http = require("http");
-var handleRequest = require("./request-handler").requestHandler;
+// var http = require("http");
+// var handleRequest = require("./request-handler").requestHandler;
 
 
-// Every server needs to listen on a port with a unique number. The
-// standard port for HTTP servers is port 80, but that port is
-// normally already claimed by another server and/or not accessible
-// so we'll use a standard testing port like 3000, other common development
-// ports are 8080 and 1337.
-var port = 3000;
+// var port = 3000;
 
-// For now, since you're running this server on your local machine,
-// we'll have it listen on the IP address 127.0.0.1, which is a
-// special address that always refers to localhost.
-var ip = "127.0.0.1";
+// var ip = "127.0.0.1";
 
 
 
-// We use node's http module to create a server.
-//
-// The function we pass to http.createServer will be used to handle all
-// incoming requests.
-//
-// After creating the server, we will tell it to listen on the given port and IP. */
-var server = http.createServer(handleRequest);
-console.log("Listening on http://" + ip + ":" + port);
-server.listen(port, ip);
+// var server = http.createServer(handleRequest);
+// console.log("Listening on http://" + ip + ":" + port);
+// server.listen(port, ip);
+/****************************************/
+var express = require("express");
+var app = express();
+var messages;
 
-// To start this server, run:
-//
-//   node basic-server.js
-//
-// on the command line.
-//
-// To connect to the server, load http://127.0.0.1:3000 in your web
-// browser.
-//
-// server.listen() will continue running as long as there is the
-// possibility of serving more requests. To stop your server, hit
-// Ctrl-C on the command line.
+function createMessage(request, callback){
+  var results = '';
+  request.on('data', function(data){
+    results += data;
+  });
+  request.on('end', function(){
+    callback(JSON.parse(results));
+  });
+}
 
+var headers = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10, // Seconds.
+  'Content-Type': "application/json"
+};
+
+app.get('/classes', function(request, response){
+  response.header(headers).status(200).send(messages);
+});
+
+app.post('/classes', function(request, response){
+  createMessage(request, function(message){
+    messages.push(message);
+    fs.appendFile('./messages.txt', JSON.stringify(message) + '\n');
+    message.objectId = ++objectId;
+    response.header(headers).status(201).send();
+  });
+});
+
+var server = app.listen(3000, function(){
+  var host = server.address().address;
+  var port = server.address().port;
+});
