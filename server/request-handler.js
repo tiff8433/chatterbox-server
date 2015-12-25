dde4c7245602d6aa7c72fc4427e356e2f2036d23
tiff8433 <tiffany.l.huang@gmail.com>
2015-12-24 18:07:20 -0800

    Meets basic requirements with app working

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,73 +1,64 @@
-/*************************************************************
-
-You should implement your request handler function in this file.
-
-requestHandler is already getting passed to http.createServer()
-in basic-server.js, but it won't work as is.
-
-You'll have to figure out a way to export this function from
-this file and include it in basic-server.js so that it actually works.
-
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
-
-**************************************************************/
-
-var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
-
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
-  console.log("Serving request type " + request.method + " for url " + request.url);
-
-  // The outgoing status.
-  var statusCode = 200;
-
-  // See the note below about CORS headers.
-  var headers = defaultCorsHeaders;
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = "text/plain";
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
-
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  response.end("Hello, World!");
-};
-
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
+var headers = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
   "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10 // Seconds.
+  "access-control-max-age": 10 ,// Seconds.
+  'Content-Type': "application/json"
 };
+
+var objectIdCounter = 0;
+var messages = [
+  // {
+  //   text: 'hi hi hi',
+  //   username: 'me', 
+  //   objectId: objectIdCounter
+  // }
+];
+
+function createMessage(request, callback){
+  var results = '';
+  request.on('data', function(data){
+    results += data;
+  });
+  request.on('end', function(){
+    callback(JSON.parse(results));
+  });
+};
+
+module.exports.sendResponse = function(response, data, statusCode){
+  statusCode = statusCode || 200;
+  response.writeHead(statusCode, headers);
+  response.end(JSON.stringify(data));
+};
+
+var actions = {
+  'GET': function(request, response){
+    module.exports.sendResponse(response, {results: messages});
+  },
+  'POST': function(request, response){
+    createMessage(request, function(message){
+      message.objectId = ++objectIdCounter;
+      messages.push(message);
+      module.exports.sendResponse(response, {objectId: message.objectId}, 201);
+    });
+  },
+  'OPTIONS': function(request, response){
+    console.log('options');
+    module.exports.sendResponse(response, null);
+  }
+};
+
+
+
+
+module.exports.requestHandler = function(request, response) {
+  var action = actions[request.method];
+  var pathName = request.url;
+  if( action && pathName.search(/(classes)/) > 0 ){
+    action(request, response);
+  } else {
+    module.exports.sendResponse(response, "Not Found", 404);
+  }                                                                                               
+}
+
 
